eb553ae88139b07fa82ab543ee65756b140ab3dc
Devanarayan J <devan23.5.2001@gmail.com>
2022-05-11 20:27:51 +0530

    little talks

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Banner } from "../components/Banner";
 import { DisplayCard } from "../components/DisplayCard";
 import tom from "../img/majortom.png";
+import littetalks from "../img/littetalks.png";
 
 export const Home = () => {
   return (
@@ -12,7 +13,7 @@ export const Home = () => {
           {/* <div className="font-ubuntu flex flex-row flex-wrap justify-center items-center -m-4 pb-12  "> */}
           <div className="font-ubuntu lg:grid lg:grid-cols-3  flex flex-col justify-center items-center">
             <DisplayCard picture={tom} tag="Space Oddity" />
-            <DisplayCard picture={tom} tag="Space Oddity" />
+            <DisplayCard picture={littetalks} tag="Little Talks" />
             <DisplayCard picture={tom} tag="Space Oddity" />
             <DisplayCard picture={tom} tag="Space Oddity" />
             <DisplayCard picture={tom} tag="Space Oddity" />
